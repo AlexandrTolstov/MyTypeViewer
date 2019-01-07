4b8097ddb81a28b2471cb1782dce52944509d8bd
AlexandrTolstov <alextav_85@mail.ru>
2019-01-07 15:28:25 +0600

    1

--- MyTypeViewer/Program.cs
+++ MyTypeViewer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace MyTypeViewer
 {
@@ -10,6 +11,82 @@ namespace MyTypeViewer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("******* Welcom to MyTypeViewer *********");
+            string typeName = "";
+            do
+            {
+                Console.WriteLine("\nEnter a type name to evaluate");
+                Console.WriteLine("or enter Q to quit: ");
+                typeName = Console.ReadLine();
+                if (typeName.ToUpper() == "Q")
+                {
+                    break;
+                }
+                try
+                {
+                    Type t = Type.GetType(typeName);
+                    Console.WriteLine();
+                    ListVariousStats(t);
+                    ListFields(t);
+                    ListProps(t);
+                    ListMethods(t);
+                    ListInterfaces(t);
+                }
+                catch
+                {
+                    Console.WriteLine("Sorry, can't find type");
+                }
+            } while (true);
+        }
+        static void ListMethods(Type t)
+        {
+            Console.WriteLine("****** Method ********");
+            MethodInfo[] mi = t.GetMethods();
+            foreach(MethodInfo m in mi)
+            {
+                string retVal = m.ReflectedType.FullName;
+                string paramInfo = " ( ";
+                foreach (ParameterInfo pi in m.GetParameters())
+                {
+                    paramInfo += string.Format("{0} {1} ", pi.ParameterType, pi.Name);
+                }
+                paramInfo += " ) ";
+                Console.WriteLine("->{0} {1} {2}", retVal, m.Name, paramInfo);
+            }
+            Console.WriteLine();
+        }
+        static void ListFields(Type t)
+        {
+            Console.WriteLine("****** Fields ********");
+            var fieldNames = from f in t.GetFields() select f.Name;
+            foreach (var name in fieldNames)
+                Console.WriteLine("->{0}", name);
+            Console.WriteLine();
+        }
+        static void ListProps(Type t)
+        {
+            Console.WriteLine("****** Fields ********");
+            var propNames = from p in t.GetProperties() select p.Name;
+            foreach (var name in propNames)
+                Console.WriteLine("->{0}", name);
+            Console.WriteLine();
+        }
+        static void ListInterfaces(Type t)
+        {
+            Console.WriteLine("****** Interfaces *********");
+            var ifaces = from i in t.GetInterfaces() select i;
+            foreach (Type i in ifaces)
+                Console.WriteLine("->{0}", i.Name);
+        }
+        static void ListVariousStats(Type t)
+        {
+            Console.WriteLine("****** Various Statistics *******");
+            Console.WriteLine("Base class is: {0}", t.BaseType);
+            Console.WriteLine("Is type abstract? {0}", t.IsAbstract);
+            Console.WriteLine("Is type sealed? {0}", t.IsSealed);
+            Console.WriteLine("Is type generic? {0}", t.IsGenericTypeDefinition);
+            Console.WriteLine("Is type a class type? {0}", t.IsClass);
+            Console.WriteLine();
         }
     }
 }
